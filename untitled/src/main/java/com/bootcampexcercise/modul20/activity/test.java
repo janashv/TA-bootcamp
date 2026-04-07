2de52af5c0151d3dcfea4bc5ad32ac46cbe9bf0b
Jana Shvets <janashvets@gmail.com>
2026-04-07 14:19:11 +0300

    activity 1

--- untitled/src/main/java/com/bootcampexcercise/modul20/activity/test.java
+++ untitled/src/main/java/com/bootcampexcercise/modul20/activity/test.java
@@ -1,4 +1,12 @@
 package com.bootcampexcercise.modul20.activity;
 
 public class test {
+    public static void main(String[] args) {
+
+        int number = 5;
+
+        number = 10;
+
+        System.out.println("your number: " + number);
+    }
 }

c6f9ebb9db80066a085f3e627f19cfd28b1a9f1a
Jana Shvets <janashvets@gmail.com>
2026-04-08 11:30:16 +0300

    module 3, acitivity 1

---
+++ untitled/src/main/java/module3/activity/Car.java
@@ -0,0 +1,16 @@
+package module3.activity;
+
+public class Car {
+
+    //attributes of car
+    String name;
+    String color;
+    String modelNo;
+
+    //constructor
+    public Car(String name, String color, String modelNo) {
+        this.name = name;
+        this.color = color;
+        this.modelNo = modelNo;
+    }
+}
